c66ab9c507994b69300472a650c81f7622f77ec4
nvan21 <153444401+nvan21@users.noreply.github.com>
2024-05-02 17:11:10 -0500

    Added actor and critic networks. Also added trajectory storage tensors

--- ppo.py
+++ ppo.py
@@ -50,6 +50,33 @@ def get_args():
     return parser.parse_args()
 
 
+# From the PPO optimization blog post github
+def init_layer(layer: nn.Linear, std=np.sqrt(2), bias_const=0.0):
+    nn.init.orthogonal_(layer.weight, std)
+    nn.init.constant_(layer.bias, bias_const)
+
+
+class Agent(nn.Module):
+    def __init__(self, envs: gym.vector.SyncVectorEnv):
+        super(Agent, self).__init__()
+
+        self.critic = nn.Sequential(
+            init_layer(nn.Linear(envs.single_observation_space.shape[0], 64)),
+            nn.Tanh(),
+            init_layer(nn.Linear(64, 64)),
+            nn.Tanh(),
+            init_layer(nn.Linear(64, 1)),
+        )
+
+        self.actor = nn.Sequential(
+            init_layer(nn.Linear(envs.single_observation_space.shape[0], 64)),
+            nn.Tanh(),
+            init_layer(nn.Linear(64, 64)),
+            nn.Tanh(),
+            init_layer(nn.Linear(64, envs.single_action_space.n)),
+        )
+
+
 def create_env(env_id, seed):
     def callback():
         env = gym.make(env_id)
@@ -74,3 +101,30 @@ if __name__ == "__main__":
     envs = gym.vector.SyncVectorEnv(
         [create_env(args.env_id, args.seed) for i in range(args.num_envs)]
     )
+
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() and args.cuda is True else "cpu"
+    )
+
+    agent = Agent(envs).to(device)
+    agent_optimizer = torch.optim.Adam(
+        agent.parameters(), lr=args.learning_rate, eps=1e-5
+    )
+
+    # Initialize storage tensors - for states and actions, each tensor has a dimension for each step.
+    # The matrix in this dimension is number of envs x observation/action space
+    # This means that each row in the printed tensor is the observation for one environment in one step
+    # It's effectively creating a 3D matrix with the following dimensions: number of steps x number of envs x space of observation/action space
+    states = torch.zeros(
+        (args.num_steps, args.num_envs) + envs.single_observation_space.shape
+    ).to(device)
+    actions = torch.zeros(
+        (args.num_steps, args.num_envs) + envs.single_action_space.shape
+    ).to(device)
+    log_probs = torch.zeros(args.num_steps, args.num_envs).to(device)
+    rewards = torch.zeros(args.num_steps, args.num_envs).to(device)
+    dones = torch.zeros(args.num_steps, args.num_envs).to(device)
+    values = torch.zeros(args.num_steps, args.num_envs).to(device)
+
+    for update in range(args.num_epochs):
+        pass
